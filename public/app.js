96ad52fed9992b34c9b5528702116d8c1f792c2f
raulnabarret <raulnabarret@gmail.com>
2016-07-15 11:06:01 -0500

    Corregir error que no permitía acceder al API de Marvel (Missing API Key)

--- public/app.js
+++ public/app.js
@@ -1,33 +1,24 @@
 $(document).ready(function () {
 
-	let url = 'http://gateway.marvel.com:80/v1/public/series?title=avengers&apikey=70c13ec2b56b84adfcd99807f11bbe74';
-	let key = 'apikey=70c13ec2b56b84adfcd99807f11bbe74';
+	var MarvelApi = window.MarvelApi;
 
-	//Callbacks
-	// Promise.resolve($.get(url, function(data) {
-	// 	 	var characters = data.data.results[0].characters.items
-	// 		debugger		
-	// 	})
-	// )
+	var key = '70c13ec2b56b84adfcd99807f11bbe74';
+	var api = new MarvelApi(key);
 
-	// Promises
-	Promise.resolve($.get(url)).then(data => {
-		var characters = data.data.results[0].characters.items;
+	api.findSeries('avengers').then(serie => {
+
+		var characters = serie.characters.items;
 		var promises = [];
 
-		for (var i in characters) {
+		for (let character of characters) {
 
-			var character = characters[i];
-			var characterUrl = `${ character.resourceURI }?${ key }`;
-
-			promises.push(Promise.resolve($.get(characterUrl)));
+			let promise = api.getResourceURI(character.resourceURI);
+			promises.push(promise);
 		}
-
 		return Promise.all(promises);
 	}).then(characters => {
 		console.log(characters);
 	}).catch(err => {
-		debugger;
 		console.error(err);
 	});
 });
